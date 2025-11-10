3daa7a66982a20e2093d913c8ba0ce066017e83a
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-11-10 16:17:23 +0800

    feat: created AR invoice offsetting

---
+++ ARAS.Blazor/Models/DTOs/ARAdjustmentCreateValidationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ARAS.Blazor.Models.DTOs
+{
+    public class ARAdjustmentCreateValidationDto
+    {
+        public string InvoiceNumber { get; set; }
+        public double AdjustmentType { get; set; }
+        public string Remarks { get; set; }
+    }
+}

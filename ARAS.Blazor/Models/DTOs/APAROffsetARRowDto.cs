84424d6734bdd892bd187b0641a62ac2719bed25
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-11-14 16:57:04 +0800

    fix: submission summary

--- ARAS.Blazor/Models/DTOs/APAROffsetARRowDto.cs
+++ ARAS.Blazor/Models/DTOs/APAROffsetARRowDto.cs
@@ -2,16 +2,9 @@
 {
     public class APAROffsetARRowDto
     {
-        // Type of row: Invoice or Adjustment
         public ARRowType RowType { get; set; } = ARRowType.Invoice;
-
-        // Invoice Fields
         public string InvoiceNumber { get; set; } = string.Empty;
-
-        // Adjustment Fields
         public string? AdjustmentReason { get; set; } = null;
-
-        // Common
         public double Amount { get; set; } = 0.00d;
     }
 

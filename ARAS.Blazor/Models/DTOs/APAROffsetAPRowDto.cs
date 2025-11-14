bcb0807df230638825dcb7ebf6de0012b8c0a220
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-11-14 15:36:29 +0800

    fix: AR grid, added create row

---
+++ ARAS.Blazor/Models/DTOs/APAROffsetAPRowDto.cs
@@ -0,0 +1,17 @@
+﻿using ARAS.Blazor.App_Code.Globals;
+using Newtonsoft.Json.Linq;
+
+namespace ARAS.Blazor.Models.DTOs
+{
+    public class APAROffsetAPRowDto
+    {
+        public double InvoiceAmount { get; set; } = 1_000.00d;
+        public string InvoiceNumber { get; set; } = string.Empty;
+
+        public APAROffsetAPRowDto(InvoiceDetailsDto details)
+        {
+            InvoiceAmount = details.InvoiceAmount;
+            InvoiceNumber = details.InvoiceNumber;
+        }
+    }
+}
--- ARAS.Blazor/Models/DTOs/APAROffsetARRowDto.cs
+++ ARAS.Blazor/Models/DTOs/APAROffsetARRowDto.cs
@@ -2,7 +2,22 @@
 {
     public class APAROffsetARRowDto
     {
+        // Type of row: Invoice or Adjustment
+        public ARRowType RowType { get; set; } = ARRowType.Invoice;
+
+        // Invoice Fields
         public string InvoiceNumber { get; set; } = string.Empty;
-        public double InvoiceAmount { get; set; } = 0.00d;
+
+        // Adjustment Fields
+        public string? AdjustmentReason { get; set; } = null;
+
+        // Common
+        public double Amount { get; set; } = 0.00d;
+    }
+
+    public enum ARRowType
+    {
+        Invoice,
+        Adjustment
     }
 }

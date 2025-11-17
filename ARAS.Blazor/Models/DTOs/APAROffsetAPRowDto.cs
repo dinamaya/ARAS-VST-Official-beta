bb061125ac63b8c7c5e01a47af8e5ac1fe20987e
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-11-17 10:51:03 +0800

    wip: work in progress

--- ARAS.Blazor/Models/DTOs/APAROffsetAPRowDto.cs
+++ ARAS.Blazor/Models/DTOs/APAROffsetAPRowDto.cs
@@ -7,6 +7,8 @@ namespace ARAS.Blazor.Models.DTOs
     {
         public double InvoiceAmount { get; set; } = 1_000.00d;
         public string InvoiceNumber { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
+        public string CustomerNumber { get; set; } = string.Empty;
 
         public APAROffsetAPRowDto(InvoiceDetailsDto details)
         {

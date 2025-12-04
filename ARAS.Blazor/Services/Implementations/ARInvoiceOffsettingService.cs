c74777daa1aafc6be3e333a1adace803467a7f3d
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-12-04 15:38:35 +0800

    feat: create interface and implementations for AR Invoice Offsetting services

---
+++ ARAS.Blazor/Services/Implementations/ARInvoiceOffsettingService.cs
@@ -0,0 +1,27 @@
+﻿using ARAS.Blazor.Services.Interfaces;
+using ARAS.Blazor.Models.DTOs;
+
+namespace ARAS.Blazor.Services.Implementations
+{
+    public class ARInvoiceOffsettingService :
+        BaseAdjustmentCommandService<ARInvoiceOffsettingCreateDto, AROffsettingRowDto, ARInvoiceOffsettingCreateValidationDto>,
+        IARInvoiceOffsettingService
+    {
+        public ARInvoiceOffsettingService(IConfigService configService, IBaseAdjustmentService<ARInvoiceOffsettingCreateDto, AROffsettingRowDto, ARInvoiceOffsettingCreateValidationDto> baseAdjustment) :
+    base(baseAdjustment, configService.GetARInvoiceOffsettingUrl(), "cdr", Map)
+        {
+        }
+
+        private static readonly Func<CashDiscountRowDto, CashDiscountCreateDto> Map = (row) => new()
+        {
+            DiscountValue = row.DiscountValue,
+            InvoiceAmount = row.InvoiceAmount,
+            InvoiceDate = DateTime.Parse(row.InvoiceDate),
+            InvoiceNumber = row.InvoiceNumber,
+            CustomerName = row.CustomerName,
+            CustomerNumber = row.CustomerNumber,
+            ReasonCode = row.ReasonCode,
+            Remarks = row.Remarks,
+        };
+    }
+}
---
+++ ARAS.Blazor/Services/Interfaces/IARInvoiceOffsettingService.cs
@@ -0,0 +1,9 @@
+﻿using ARAS.Blazor.Models.DTOs;
+using ARAS.Blazor.Repositories.Interfaces;
+
+namespace ARAS.Blazor.Services.Interfaces
+{
+    public interface IARInvoiceOffsettingService : IBaseAdjustmentCommandRepository<AROffsettingRowDto, ARInvoiceOffsettingCreateDto, ARInvoiceOffsettingCreateValidationDto>
+    {
+    }
+}

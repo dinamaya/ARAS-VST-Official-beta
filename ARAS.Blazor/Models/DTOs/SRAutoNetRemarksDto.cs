106dbc5b8b4470c01e20e82c66a0b53d15cd1425
Kenneth Napura <senpai24kenjie@gmail.com>
2025-12-10 21:52:50 +0800

    Commits

---
+++ ARAS.Blazor/Models/DTOs/SRAutoNetRemarksDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ARAS.Blazor.Models.DTOs
+{
+	public class SRAutoNetRemarksDto
+	{
+		public string CNRef { get; set; } = string.Empty;
+		public string CNAmt { get; set; } = string.Empty;
+		public string WT { get; set; } = string.Empty;
+	}
+}
---
+++ ARAS.Blazor/Models/DTOs/SRAutoNetRowDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ARAS.Blazor.Models.DTOs
+{
+	public class SRAutoNetRowDto
+	{
+		public string Id { get; set; }
+		public string GLDate { get; set; }
+		public double AdjustmentAmount { get; set; }
+		public string AdjustmentActivity { get; set; } = string.Empty;
+		public string InvoiceNumber { get; set; } = string.Empty;
+		public string CustomerName { get; set; } = string.Empty;
+		public string CustomerNumber { get; set; } = string.Empty;
+		public string ReasonCode { get; set; } = string.Empty;
+		public SRAutoNetRemarksDto Remarks { get; set; }
+	}
+}

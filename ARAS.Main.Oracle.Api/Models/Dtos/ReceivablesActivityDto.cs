3e56f8bf81e7d5c30f5bf334063c4324ac115070
Kenneth Napura <senpai24kenjie@gmail.com>
2025-12-18 18:18:48 +0800

    feature: oracle api - implemented fetching of receivable activities and positng in Oracle DB

---
+++ ARAS.Main.Oracle.Api/Models/Dtos/ReceivablesActivityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ARAS.Main.Oracle.Api.Models.Dtos
+{
+	public class ReceivablesActivityDto
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+	}
+}
--- ARAS.Main.Oracle.Api/Repositories/Implementations/AdjustmentRepository.cs
+++ ARAS.Main.Oracle.Api/Repositories/Implementations/AdjustmentRepository.cs
@@ -1,6 +1,8 @@
 ﻿using ARAS.Main.Oracle.Api.App_Code.Globals.Constants;
 using ARAS.Main.Oracle.Api.Context;
 using ARAS.Main.Oracle.Api.Factories.Interfaces;
+using ARAS.Main.Oracle.Api.Models.Dtos;
+using ARAS.Main.Oracle.Api.Models.Entities;
 using ARAS.Main.Oracle.Api.Repositories.Interfaces;
 using ARAS.Main.Oracle.Api.Services.Interfaces;
 using Dapper;
@@ -51,6 +53,90 @@ namespace ARAS.Main.Oracle.Api.Repositories.Implementations
 				throw new InvalidOperationException(Exceptions.NULL_REASON_CODES);
 
 			return list;
+		}
+
+		public async Task<IEnumerable<ReceivablesActivityDto>> GetReceivableActivities()
+		{
+			var conn = await oracleConnection.OpenWithoutPolicyAsync();
+
+			var sql = @"
+				SELECT   
+					RECEIVABLES_TRX_ID Id,
+					NAME Name
+				FROM   ar_receivables_trx_all
+				WHERE   TYPE = 'ADJUST'
+			";
+
+			var result = await conn.QueryAsync<ReceivablesActivityDto>(sql);
+			return result;
+		}
+
+		public async Task Create(AdjustmentPostingDto data)
+		{
+			await using var conn = await oracleConnection.OpenWithoutPolicyAsync();
+
+			var sql = @"
+					INSERT INTO APPS.XXMSI_AR_ADJ_STG
+					(
+						HEADER_ID,
+						CUSTOMER_TRX_ID,
+						INVOICE_NUMBER,
+						AMOUNT,
+						CREATED_FROM,
+						GL_DATE,
+						TYPE,
+						PAYMENT_SCHEDULE_ID,
+						APPLY_DATE,
+						RECEIVABLES_TRX_ID,
+						REASON_CODE,
+						COMMENTS,
+						ACCOUNT_NAME,
+						ACCOUNT_NUMBER,
+						STG_FLAG,
+						INT_FLAG,
+						INV_FLAG
+					)
+					VALUES(
+						:AdjustmentId,
+						:CustomerTRXId,
+						:InvoiceNumber,
+						:AdjustmentAmount,
+						'ADJUSTMENT API',
+						:GLDate,
+						'LINE',
+						:PaymentScheduleId, -- IS IT NULL?
+						:DateApplied,
+						:TransactionTypeId,
+						:ReasonCode,
+						:Remarks,
+						:AccountName,
+						:AccountNumber,
+						1,
+						0,
+						0
+					)
+				";
+
+			var param = new {
+				AdjustmentId = data.AdjustmentId,
+				CustomerTRXId = data.CustomerTRXId,
+				InvoiceNumber = data.InvoiceNumber,
+				AdjustmentAmount = data.AdjustmentAmount,
+				GLDate = data.GLDate,
+				PaymentScheduleId = data.PaymentScheduleId,
+				DateApplied = data.DateApplied,
+				TransactionTypeId = data.TransactionTypeId,
+				ReasonCode = data.ReasonCode,
+				Remarks = data.Remarks,
+				AccountName = data.AccountName,
+				AccountNumber = data.AccountNumber
+			};
+
+			await conn.ExecuteAsync(
+				sql,
+				param,
+				commandTimeout: 120
+			);
 		}
 	}
 }

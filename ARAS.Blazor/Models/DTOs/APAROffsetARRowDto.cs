a6b215d5a0f29718d5887c50ca4d4a3afed553fd
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2026-01-15 10:22:02 +0800

    fix: modified APAROffsetARROwDto to add to the AR Group thru fetching from ERP

--- ARAS.Blazor/Models/DTOs/APAROffsetARRowDto.cs
+++ ARAS.Blazor/Models/DTOs/APAROffsetARRowDto.cs
@@ -4,10 +4,25 @@
     {
         public string Id { get; set; }
         public ARRowType RowType { get; set; } = ARRowType.Invoice;
+        public double InvoiceAmount { get; set; } = 1_000.00d;
         public string InvoiceNumber { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
+        public string CustomerNumber { get; set; } = string.Empty;
         public string? AdjustmentReason { get; set; } = null;
         public double Amount { get; set; } = 0.00d;
+
+        public APAROffsetARRowDto() { }
+
+        public APAROffsetARRowDto(InvoiceDetailsDto details)
+        {
+            Id = details.Id;
+            Amount = details.InvoiceAmount;
+            InvoiceNumber = details.InvoiceNumber;
+            CustomerName = details.CustomerName;
+            CustomerNumber = details.CustomerNumber;
+        }
     }
+
 
     public enum ARRowType
     {

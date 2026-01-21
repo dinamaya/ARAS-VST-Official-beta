602fbfa0f3b6f654bf7fec34794c0b2932b06a89
Kenneth Napura <senpai24kenjie@gmail.com>
2026-01-21 16:28:01 +0800

    -stable version

--- ARAS.Main.SSMS.Api/Repositories/Interfaces/IBaseAdjustmentRepository.cs
+++ ARAS.Main.SSMS.Api/Repositories/Interfaces/IBaseAdjustmentRepository.cs
@@ -5,20 +5,19 @@ namespace ARAS.Main.SSMS.Api.Repositories.Interfaces
 	public interface IBaseAdjustmentRepository<TCreate>
 	{
 		Task<long> Create(
-			RequestCreationDto<AdjustmentRequestCreationDto<TCreate>> data,
+			RequestCreationDto<TCreate> data,
 			string createdBy,
 			string adjustmentTypeCode,
 			Func<TCreate, string, long, Task> createInvoiceCallBack);
 		Task<long> Update(
 			long requestId, 
-			RequestCreationDto<AdjustmentRequestCreationDto<TCreate>> data, 
+			RequestCreationDto<TCreate> data, 
 			string modifiedBy,
 			string adjustmentTypeCode,
 			string adjustmentRouteName,
 			Func<TCreate, string, long, Task> updateInvoiceCallBack);
 
 		Task Approve(RequestUpdateDto data, string createdBy, string adjustmentTypeName);
-		Task Validate(RequestUpdateDto data, string createdBy, string adjustmentTypeName);
 		Task Decline(NegateRequestDto data, string createdBy, string adjustmentTypeName);
 		Task Reject(NegateRequestDto data, string createdBy, string adjustmentTypeName);
 	}

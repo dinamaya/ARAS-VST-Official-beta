eadccde5af9a308f2ae8829fba6cddad3514c698
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2026-01-21 10:44:42 +0800

    feat: created Dtos for AR Invoice Offsetting, AR and CN groups

---
+++ ARAS.Main.SSMS.Api/Models/Dtos/ARInvoiceOffsettingCNDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ARAS.Main.SSMS.Api.Models.Dtos
+{
+    public class ARInvoiceOffsettingCNDto
+    {
+        public double InvoiceAmount { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerNumber { get; set; }
+        public string Remarks { get; set; }
+    }
+}
--- ARAS.Main.SSMS.Api/Models/Dtos/ARInvoiceOffsettingCreateDto.cs
+++ ARAS.Main.SSMS.Api/Models/Dtos/ARInvoiceOffsettingCreateDto.cs
@@ -1,12 +1,7 @@
 ﻿namespace ARAS.Main.SSMS.Api.Models.Dtos
 {
-    public class ARInvoiceOffsettingCreateDto
+    public class ARInvoiceOffsettingCreateDto : InvoiceCreateDto
     {
-        public double InvoiceAmount { get; set; }
-        public DateTime InvoiceDate { get; set; }
-        public string InvoiceNumber { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerNumber { get; set; }
-        public string Remarks { get; set; }
+        public IList<ARInvoiceOffsettingCNDto> Remarks { get; set; }
     }
 }

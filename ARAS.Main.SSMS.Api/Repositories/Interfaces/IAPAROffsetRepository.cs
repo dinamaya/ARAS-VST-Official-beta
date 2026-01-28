f87bf56c862aaaac0087d60c12326f35c235d183
Kenneth Napura <senpai24kenjie@gmail.com>
2026-01-28 14:44:58 +0800

    fixed: restored apar services and adjustments

--- ARAS.Main.SSMS.Api/Repositories/Interfaces/IAPAROffsetRepository.cs
+++ ARAS.Main.SSMS.Api/Repositories/Interfaces/IAPAROffsetRepository.cs
@@ -4,5 +4,6 @@ namespace ARAS.Main.SSMS.Api.Repositories.Interfaces
 {
     public interface IAPAROffsetRepository : ICreateReceiptStatusRepository<APAROffsetCreateDto, long>
 	{
-    }
+		Task<APAROffsetRowDto> GetAPAdjustmentsByRequestId(long requestId);
+	}
 }

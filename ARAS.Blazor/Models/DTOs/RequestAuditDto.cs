5929da6f6d0f0ace15d216f13be0854f86a819ce
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2026-03-06 11:40:38 +0800

    fix: updated Dto's to align with the database changes

--- ARAS.Blazor/Models/DTOs/RequestAuditDto.cs
+++ ARAS.Blazor/Models/DTOs/RequestAuditDto.cs
@@ -3,9 +3,10 @@
 	public class RequestAuditDto
 	{
 		public string Requestor { get; set; }
-		public string? Approver { get; set; }
-		public string? Validator { get; set; }
-		public string? Checker { get; set; }
+        public string? CNCApprover { get; set; }
+        public string? FSGValidator { get; set; }
+        public string? FSGApprover { get; set; }
+        public string? Checker { get; set; }
 		public string? ReferenceNumber { get; set; }
 		public string? DateRequested { get; set; }
 
@@ -13,9 +14,10 @@
 		public RequestAuditDto(TransactionRequestRowDto transactionRequestRow) 
 		{
 			Requestor = transactionRequestRow.Requestor;
-			Approver = transactionRequestRow.Approver;
-			Validator = transactionRequestRow.Validator;
-			Checker = transactionRequestRow.Creator;
+            CNCApprover = transactionRequestRow.CNCApprover;
+            FSGValidator = transactionRequestRow.FSGValidator;
+            FSGApprover = transactionRequestRow.FSGApprover;
+            Checker = transactionRequestRow.Creator;
 			ReferenceNumber = transactionRequestRow.RequestNumber;
 			DateRequested = transactionRequestRow.DateRequested;
 		}
--- ARAS.Blazor/Models/DTOs/TransactionRequestRowDto.cs
+++ ARAS.Blazor/Models/DTOs/TransactionRequestRowDto.cs
@@ -6,11 +6,13 @@
 		public string RequestNumber { get; set; }
 		public string Requestor { get; set; }
 		public string DateRequested { get; set; }
-		public string Approver { get; set; }
-		public string DateApproved { get; set; }
-		public string Validator { get; set; }
-		public string DateValidated { get; set; }
-		public string Creator { get; set; }
+        public string CNCApprover { get; set; }
+        public string DateCNCApproved { get; set; }
+        public string FSGValidator { get; set; }
+        public string DateFSGValidated { get; set; }
+        public string FSGApprover { get; set; }
+        public string DateFSGApproved { get; set; }
+        public string Creator { get; set; }
 		public string DateCreated { get; set; }
 		public string Status { get; set; }
 	}

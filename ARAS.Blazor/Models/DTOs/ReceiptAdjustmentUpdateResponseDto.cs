28a80a592b918e1ecac3d245879fae52fb10d1a2
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2026-03-25 17:52:23 +0800

    fix:  temporary SSMS API marker

--- ARAS.Blazor/Models/DTOs/ReceiptAdjustmentUpdateResponseDto.cs
+++ ARAS.Blazor/Models/DTOs/ReceiptAdjustmentUpdateResponseDto.cs
@@ -4,6 +4,7 @@
     {
 		public double AdjustmentAmount { get; set; }
 		public string Remarks { get; set; }
+		public string ApiMarker { get; set; }
 		public InvoiceDetailsRequestDto Invoice { get; set; }
 
 		public IEnumerable<TransactionHistoryDto> TransactionHistories { get; set; }
